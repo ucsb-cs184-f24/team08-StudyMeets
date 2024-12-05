bdd83eeacd4bb68fb38e71d33aca474269fab018
garvinwyoung <garvinwyoung@ucsb.edu>
2024-12-05 12:40:02 -0800

    added new profile fields and allowed editing

--- StudyMeets/app/screens/SettingsTab.js
+++ StudyMeets/app/screens/SettingsTab.js
@@ -3,10 +3,22 @@ import { View, TouchableOpacity, Text, StyleSheet } from "react-native";
 import { useNavigation } from '@react-navigation/native';
 import { Mail } from "lucide-react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { auth } from '../../firebase';
+import { Button as PaperButton } from 'react-native-paper';
+import { signOut } from "firebase/auth";
 
 const SettingsTab = () => {
     
     const navigation = useNavigation();
+
+    const handleLogout = async () => {
+      try {
+        await signOut(auth);
+        navigation.navigate('Login');
+      } catch (error) {
+        console.error("Error logging out:", error);
+      }
+    };
 
     return (
         <SafeAreaView style={styles.container}>
@@ -17,23 +29,33 @@ const SettingsTab = () => {
             <Mail size={40} color="black" />
           </TouchableOpacity>
 
-          <TouchableOpacity
-            style={styles.settingOption}
-            onPress={() => navigation.navigate('Notification Settings')}>
-            <Text style={styles.settingText}>Notification Settings</Text>
-          </TouchableOpacity>
-    
-          <TouchableOpacity
-            style={styles.settingOption}
-            onPress={() => navigation.navigate('Profile Privacy Settings')}>
-            <Text style={styles.settingText}>Profile Privacy Settings</Text>
-          </TouchableOpacity>
-    
-          <TouchableOpacity
-            style={styles.settingOption}
-            onPress={() => navigation.navigate('General Settings')}>
-            <Text style={styles.settingText}>General Settings</Text>
-          </TouchableOpacity>
+          <View style={styles.settingsContainer}>
+            <TouchableOpacity
+              style={styles.settingOption}
+              onPress={() => navigation.navigate('Notification Settings')}>
+              <Text style={styles.settingText}>Notification Settings</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              style={styles.settingOption}
+              onPress={() => navigation.navigate('Profile Privacy Settings')}>
+              <Text style={styles.settingText}>Profile Privacy Settings</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              style={styles.settingOption}
+              onPress={() => navigation.navigate('General Settings')}>
+              <Text style={styles.settingText}>General Settings</Text>
+            </TouchableOpacity>
+          </View>
+
+          <PaperButton
+            mode="outlined"
+            onPress={handleLogout}
+            style={styles.logoutButton}
+          >
+            Logout
+          </PaperButton>
 
         </SafeAreaView>
     );
@@ -46,16 +68,20 @@ const styles = StyleSheet.create({
         flex: 1,
         paddingTop: 20,
         paddingHorizontal: 15,
+        justifyContent: 'space-between',
     },
     title: {
         fontSize: 30,
         fontWeight: 'bold',
         marginBottom: 20,
     },
-        mailIcon: {
+    mailIcon: {
         position: 'absolute',
         top: 45,
         right: 15,
+    },
+    settingsContainer: {
+        flex: 1,
     },
     settingOption: {
         backgroundColor: '#fff',
@@ -70,5 +96,7 @@ const styles = StyleSheet.create({
         fontSize: 16,
         color: '#000',
     },
+    logoutButton: {
+        marginVertical: 20,
+    },
 });
-    
